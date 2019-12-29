5f37e3490cb0d1d2a0bad3ba9aaeb045436ae7dc
lch-howger <360000521@qq.com>
2019-12-29 18:18:23 +0000

    finish select time

--- views/js/addition.js
+++ views/js/addition.js
@@ -36,9 +36,9 @@ function addBook() {
 function addBookById() {
     let bookid = document.querySelector('#input_book_id').value;
     let query_url = href + '/' + bookid;
-    params = {};
+    let params = {};
     if (first_path == 'users') {
-        let book_due_date = document.querySelector('#input_due_date').value;
+        let book_due_date = getDueDateBySelect();
         params = {
             dueDate: book_due_date,
         };
@@ -56,7 +56,7 @@ function addBookById() {
 function addNewBook() {
     let book_title = document.querySelector('#input_book_title').value;
     let query_url = href;
-    params = {};
+    let params = {};
     if (first_path == 'authors') {
         let book_isbn = document.querySelector('#input_book_isbn').value;
         params = {
@@ -76,4 +76,64 @@ function addNewBook() {
         .catch(function (error) {
             alert(error.toString());
         });
+}
+
+function initSelectTime() {
+    if (first_path == 'users') {
+        let select_year = document.querySelector('#select_year');
+        for (let i = 2000; i < 2100; i++) {
+            let option = document.createElement('option');
+            option.setAttribute('class', 'option_year');
+            option.setAttribute('value', i);
+            option.appendChild(document.createTextNode(i));
+            select_year.appendChild(option);
+        }
+
+        let select_month = document.querySelector('#select_month');
+        for (let i = 1; i < 13; i++) {
+            let option = document.createElement('option');
+            option.setAttribute('class', 'option_month');
+            option.setAttribute('value', i);
+            option.appendChild(document.createTextNode(i));
+            select_month.appendChild(option);
+        }
+
+        let select_day = document.querySelector('#select_day');
+        for (let i = 1; i < 32; i++) {
+            let option = document.createElement('option');
+            option.setAttribute('class', 'option_day');
+            option.setAttribute('value', i);
+            option.appendChild(document.createTextNode(i));
+            select_day.appendChild(option);
+        }
+    }
+}
+
+initSelectTime();
+
+function getDueDateBySelect() {
+    let y = 2000;
+    let m = 1;
+    let d = 1;
+    let years = document.querySelectorAll('.option_year');
+    years.forEach(function (year) {
+        if (year.selected) {
+            y = year.value;
+        }
+    });
+
+    let months = document.querySelectorAll('.option_month');
+    months.forEach(function (month) {
+        if (month.selected) {
+            m = month.value;
+        }
+    });
+
+    let days = document.querySelectorAll('.option_day');
+    days.forEach(function (day) {
+        if (day.selected) {
+            d = day.value;
+        }
+    });
+    return '' + y + '-' + m + '-' + d;
 }
