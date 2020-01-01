9faeeece6da81d299888b53638de26fa569463dc
lch-howger <360000521@qq.com>
2020-01-01 21:40:39 +0000

    comment

--- routes/loans.js
+++ routes/loans.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const router = express.Router();
-
 const db = require("../data");
 const ret = require("../lib/return");
 
+/**
+ * get /
+ * enter loans page
+ * show loan list
+ */
 router.get("/", function (req, res) {
     res.sendfile("./dist/loans.html");
 });
 
+/**
+ * get /list
+ * return loan list
+ */
 router.get("/list", function(req, res) {
     db.Loan.findAll().then(function(loans) {
         ret.json(loans, res);
     });
 });
 
+/**
+ * get /:loanID
+ * return loan detail by loan ID
+ */
 router.get("/:loanID", function(req, res) {
     if (req.query.allEntities == "true") {
         db.Loan.findByPk(req.params.loanID).then(function (loan) {
@@ -28,10 +40,18 @@ router.get("/:loanID", function(req, res) {
     }
 });
 
+/**
+ * get /:loanID/change
+ * enter change page
+ */
 router.get("/:loanID/change", function (req, res) {
     res.sendfile("./dist/add.html");
 });
 
+/**
+ * put /:loanID
+ * change loan by loan ID
+ */
 router.put("/:loanID", function(req, res) {
     db.Loan.findByPk(req.params.loanID).then(function(loan) {
         if (loan) {
@@ -45,6 +65,10 @@ router.put("/:loanID", function(req, res) {
     });
 });
 
+/**
+ * delete /:loanID
+ * delete loan by loan ID
+ */
 router.delete("/:loanID", function(req, res) {
     db.Loan.findByPk(req.params.loanID)
         .then(function(loan) {
