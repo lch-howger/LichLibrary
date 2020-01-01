69d2aec2c570459bbc963fd8fd1b4430533d4b5a
lch-howger <360000521@qq.com>
2020-01-01 21:28:07 +0000

    comment

--- routes/authors.js
+++ routes/authors.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
-
 const db = require("../data");
 const ret = require("../lib/return");
 
+/*
+get /
+enter authors pagedz
+ */
 router.get("/", function (req, res) {
     res.sendfile("./dist/authors.html");
 });
 
+/*
+get /list
+return author list in json
+if allEntities=='true' return book list
+ */
 router.get("/list", function (req, res) {
     if (req.query.allEntities == "true") {
         db.Author.findAll({include: [db.Book]}).then(function (authors) {
@@ -20,10 +28,18 @@ router.get("/list", function (req, res) {
     }
 });
 
+/*
+get /add
+enter add page
+ */
 router.get("/add", function (req, res) {
     res.sendfile("./dist/add.html");
 });
 
+/*
+get /:authorID
+return detail information according to author ID
+ */
 router.get("/:authorID", function (req, res) {
     if (req.query.allEntities == "true") {
         db.Author.findByPk(req.params.authorID, {include: [db.Book]}).then(function (author) {
@@ -46,20 +62,43 @@ router.get("/:authorID", function (req, res) {
     }
 });
 
+/*
+/:authorID/change
+enter add author page
+ */
 router.get("/:authorID/change", function (req, res) {
     res.sendfile("./dist/add.html");
 });
 
+/*
+/:authorID/books
+enter addition page
+add books for author
+ */
 router.get("/:authorID/books", function (req, res) {
     res.sendfile("./dist/authors_addition.html");
 });
 
+/*
+post /
+create new author
+params:
+    1.name
+    2.imgUrl
+ */
 router.post("/", function (req, res) {
     db.Author.create({name: req.body.name, imgUrl: req.body.imgUrl}).then(function (author) {
         ret.json(author, res);
     });
 });
 
+/*
+post /:authorID/books
+add new book for author
+params:
+    1.bookTitle
+    2.bookISBN
+ */
 router.post("/:authorID/books", function (req, res) {
     db.Author.findByPk(req.params.authorID, {include: [db.Book]}).then(function (author) {
         if (author) {
@@ -77,6 +116,10 @@ router.post("/:authorID/books", function (req, res) {
     });
 });
 
+/*
+post /:authorID/books/:bookID
+add book for author by book ID
+ */
 router.post("/:authorID/books/:bookID", function (req, res) {
     db.Author.findByPk(req.params.authorID, {include: [db.Book]}).then(function (author) {
         if (author) {
@@ -94,6 +137,13 @@ router.post("/:authorID/books/:bookID", function (req, res) {
     });
 });
 
+/*
+put /:authorID
+change author by author ID
+params:
+    1.name
+    2.imgUrl
+ */
 router.put("/:authorID", function (req, res) {
     db.Author.findByPk(req.params.authorID).then(function (author) {
         if (author) {
@@ -108,6 +158,10 @@ router.put("/:authorID", function (req, res) {
     });
 });
 
+/*
+delete /:authorID
+delete author by author ID
+ */
 router.delete("/:authorID", function (req, res) {
     db.Author.findByPk(req.params.authorID)
         .then(function (author) {
